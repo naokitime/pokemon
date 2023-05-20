565c0d426b7b07e187364caf7e69c3a413ebd48e
Hata Naoki <86907715+naokitime@users.noreply.github.com>
2023-05-20 11:16:44 +0900

    Add files via upload

---
+++ pokemon-app/src/App.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setpokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      setNextURL(res.next);
+      setPrevURL(res.previous);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setpokemonData(_pokemonData);
+  };
+
+  const handlePrevPage = async () => {
+    if (!prevURL) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>
+            ロード中<span className="loadingDot">...</span>
+          </h1>
+        ) : (
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon}></Card>;
+            })}
+          </div>
+        )}
+        {!loading && (
+          <div className="btn">
+            <button onClick={handlePrevPage}>前へ</button>
+            <button onClick={handleNextPage}>次へ</button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ pokemon-app/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+
+const Navbar = () => {
+  return <nav>ポケモン図鑑</nav>;
+};
+
+export default Navbar;
